997b6855317afac24862eeb0e235dc5c12a32811
Igor Stępień <igor@istepien.dev>
2024-01-30 02:10:23 +0100

    Database uninstall

--- src/Handler/UninstallationHandler.php
+++ src/Handler/UninstallationHandler.php
@@ -5,25 +5,59 @@ declare(strict_types=1);
 namespace Oksydan\Falconize\Handler;
 
 use Oksydan\Falconize\Connection\ConnectionManager;
-use Oksydan\Falconize\Helper\TableNameFormatterInterface;
+use Oksydan\Falconize\Database\Builder\SchemaBuilder;
+use Oksydan\Falconize\Database\Collection\DatabaseQueryCollection;
+use Oksydan\Falconize\Database\Comparator\SchemaComparator;
+use Oksydan\Falconize\Database\DTO\DatabaseQuery;
+use Oksydan\Falconize\Database\DTO\Table;
+use Oksydan\Falconize\Database\Executor\DatabaseQueryExecutor;
+use Oksydan\Falconize\Database\Retriever\DatabaseSchemaRetriever;
 use Oksydan\Falconize\Yaml\DTO\ParsedResult;
 
 class UninstallationHandler implements HandlerInterface
 {
     private ConnectionManager $connectionManager;
+    private DatabaseQueryExecutor $databaseQueryExecutor;
 
-    private TableNameFormatterInterface $tableNameFormatter;
+    private SchemaComparator $schemaComparator;
+
+    private SchemaBuilder $schemaBuilder;
+
+    private DatabaseSchemaRetriever $databaseSchemaRetriever;
 
     public function __construct(
         ConnectionManager $connectionManager,
-        TableNameFormatterInterface $tableNameFormatter
+        DatabaseQueryExecutor $databaseQueryExecutor,
+        SchemaComparator $schemaComparator,
+        SchemaBuilder $schemaBuilder,
+        DatabaseSchemaRetriever $databaseSchemaRetriever
     ) {
         $this->connectionManager = $connectionManager;
-        $this->tableNameFormatter = $tableNameFormatter;
+        $this->databaseQueryExecutor = $databaseQueryExecutor;
+        $this->schemaComparator = $schemaComparator;
+        $this->schemaBuilder = $schemaBuilder;
+        $this->databaseSchemaRetriever = $databaseSchemaRetriever;
     }
 
     public function handle(ParsedResult $config): void
     {
-        // TODO: Implement handle() method.
+        $connection = $this->connectionManager->getConnection();
+        $tables = $config->getDatabaseCollection();
+        $queryCollection = new DatabaseQueryCollection();
+
+        /** @var Table $table */
+        foreach ($tables as $table) {
+            $fromSchema = $this->databaseSchemaRetriever->getTableSchema($table);
+            $toSchema = new Schema();
+            $diff = $this->schemaComparator->compare($fromSchema, $toSchema);
+
+            $queries = $diff->toSql($connection->getDatabasePlatform());
+
+            foreach ($queries as $query) {
+                $queryCollection->add(new DatabaseQuery($query));
+            }
+        }
+
+        $this->databaseQueryExecutor->execute($queryCollection);
     }
 }
